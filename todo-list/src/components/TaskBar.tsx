a211718862b59b6638641aaeadafe9af848f36ac
hevelinlima <hevelin25@gmail.com>
2024-03-28 00:38:10 -0300

    estilização da taskbar

--- todo-list/src/components/TaskBar.tsx
+++ todo-list/src/components/TaskBar.tsx
@@ -1,8 +1,20 @@
+import styles from './TaskBar.module.css'
+
+import plusSign from '../assets/plus-sign.svg'
+
 export function TaskBar( ){
   return(
-    <div>
-      <form action=""></form>
-      <button>Criar</button>
+    <div className={styles.taskbar}>
+      <form className={styles.taskbarForm} action="">
+        <textarea name="" id="" placeholder="Adicione uma nova tarefa"></textarea>
+        <div>
+          <button type="submit">
+            Criar
+            <img src={ plusSign } alt="" />
+          </button>
+        </div>
+      </form>
+      
     </div>
   )
 }
