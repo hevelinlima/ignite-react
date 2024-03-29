43a38addd396ee3ed4427d7fbaf522828344cb3b
hevelinlima <hevelin25@gmail.com>
2024-03-28 23:10:42 -0300

    estilização das tasks pt. 1

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.module.css'
 import { Header } from './components/Header'
 import { TaskBar } from './components/TaskBar'
+import { Tasks } from './components/Tasks'
 
 export function App() {
 
   return (
     <div>
       <Header />
-
       <TaskBar />
+      <Tasks/>
     </div>
   )
 }
