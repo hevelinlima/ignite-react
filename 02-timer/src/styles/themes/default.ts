236080a26a1fe4c881d925f2753ced913f10a1ac
hevelinlima <hevelin25@gmail.com>
2024-04-11 17:34:36 -0300

    estilização da Home page

--- 02-timer/src/styles/themes/default.ts
+++ 02-timer/src/styles/themes/default.ts
@@ -6,7 +6,7 @@ export const defaultTheme = {
   'gray-400': '#8d8d99',
   'gray-500': '#7c7c8a',
   'gray-600': '#323238',
-  'gray-700': '29292e',
+  'gray-700': '#29292e',
   'gray-800': '#202024',
   'gray-900': '#121214',
 
